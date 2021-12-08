b936b9a0ffbcd7b7e474c981c978e0339363f9b6
weak1337 <57501119+weak1337@users.noreply.github.com>
2021-12-08 16:29:23 +0100

    Add files via upload

---
+++ NO_ACESS Protect/NO_ACESS Protect/main.cpp
@@ -0,0 +1,9 @@
+#include "protect.h"
+
+int main() {
+	protect::initialize();
+	/*
+		main code
+	*/
+	system("pause");
+}
---
+++ NO_ACESS Protect/NO_ACESS Protect/protect.h
@@ -0,0 +1,8 @@
+#pragma once
+#include <Windows.h>
+#include <iostream>
+#include <vector>
+#include <winternl.h>
+namespace protect {
+	void initialize();
+}
